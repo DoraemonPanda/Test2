d12576dd119a8fc4d6966211c076ab9a9ab70983
DoraemonPanda <135143975+DoraemonPanda@users.noreply.github.com>
2023-06-01 20:41:21 +0530

    Update App.java

--- Test2/src/main/java/com/example/Test2/App.java
+++ Test2/src/main/java/com/example/Test2/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "this is my first proect" );
+        System.out.println( "this is test 2" );
     }
 }
